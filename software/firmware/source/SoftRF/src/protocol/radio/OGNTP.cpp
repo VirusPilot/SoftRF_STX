2c90dd9f14c67e2a2e1bd1eba78c57f6bf01b912
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2023-02-09 21:50:11 +0100

    increase GSA, GSV to 1 Hz

--- software/firmware/source/SoftRF/src/protocol/radio/OGNTP.cpp
+++ software/firmware/source/SoftRF/src/protocol/radio/OGNTP.cpp
@@ -97,12 +97,13 @@ bool ogntp_decode(void *pkt, ufo_t *this_aircraft, ufo_t *fop) {
     return false;
   }
 
-#if !defined(SOFTRF_ADDRESS)
-  uint8_t addr_type = ADDR_TYPE_ANONYMOUS;
-#else
-  uint8_t addr_type = (this_aircraft->addr == SOFTRF_ADDRESS ?
-                        ADDR_TYPE_ICAO : ADDR_TYPE_ANONYMOUS);
-#endif
+  uint8_t addr_type;
+  if (settings->aircraft_id == 0) {
+     addr_type = ADDR_TYPE_ANONYMOUS;
+  }
+  else {
+     addr_type = ADDR_TYPE_ICAO;
+  }
 
   /* ignore this device own (relayed) packets */
   if ((ogn_rx_pkt.Packet.Header.Address    == this_aircraft->addr) &&
@@ -165,12 +166,12 @@ size_t ogntp_encode(void *pkt, ufo_t *this_aircraft) {
   ogn_tx_pkt.Packet.HeaderWord      = 0;
   ogn_tx_pkt.Packet.Header.Address  = this_aircraft->addr;
 
-#if !defined(SOFTRF_ADDRESS)
-  ogn_tx_pkt.Packet.Header.AddrType = ADDR_TYPE_ANONYMOUS;
-#else
-  ogn_tx_pkt.Packet.Header.AddrType = (this_aircraft->addr == SOFTRF_ADDRESS ?
-                                      ADDR_TYPE_ICAO : ADDR_TYPE_ANONYMOUS);
-#endif
+  if (settings->aircraft_id == 0) {
+     ogn_tx_pkt.Packet.Header.AddrType = ADDR_TYPE_ANONYMOUS;
+  }
+  else {
+     ogn_tx_pkt.Packet.Header.AddrType = ADDR_TYPE_ICAO;
+  }
 
 #if defined(USE_OGN_ENCRYPTION)
   if (key[0] || key[1] || key[2] || key[3])
